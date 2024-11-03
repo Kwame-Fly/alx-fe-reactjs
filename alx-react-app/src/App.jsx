ee82819d48a0c6b6e131e411537c2acab737a075
kwame takyi-kodua <kwametkodua@gmail.com>
2024-11-03 22:23:04 +0000

    userProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header ';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -16,6 +17,7 @@ function App() {
       <div>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
       <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">

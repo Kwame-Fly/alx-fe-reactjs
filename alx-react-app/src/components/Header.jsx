1a076dda79149e5ba4058d1d912df22269064018
kwame takyi-kodua <kwametkodua@gmail.com>
2024-11-10 13:57:45 +0000

    newly styled

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,6 +1,5 @@
 function Header () {
     return (
-
              <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
             <h1>My Favorite Cities</h1>
         </header>

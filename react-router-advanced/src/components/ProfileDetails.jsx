dfaac15662a04f0fbccee43d12975c81df76b1b9
kwame takyi-kodua <kwametkodua@gmail.com>
2024-11-23 20:52:05 +0000

    newly created files

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const ProfileDetails = () => <div>Here are your profile details.</div>;
+export default ProfileDetails;
+
+
+
---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const ProfileSettings = () => <div>Here you can change your settings.</div>;
+export default ProfileSettings;
